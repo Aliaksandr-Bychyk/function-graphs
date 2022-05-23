6ae9fd276af88e5bf5f1108ad80cd42e9b4071ba
Alexander Bychyk <sanya55bychyk@gmail.com>
2022-05-23 07:36:53 +0300

    feat: start function-graphs

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import "./styles/styles.css";
+import "./assets/svg/favicon.svg"
+
+// let a = 2;
+// let b = 2;
+// let l = 1;
+// let y = x => Math.cos(x); // функция
+// let z = 40; // масштаб
+// let c = document.querySelector('canvas');
+// let ctx = c.getContext('2d');
+
+// // центровочка
+// ctx.translate(c.width/2, c.height/2)
+   
+// // направляющие
+// // x
+// ctx.lineWidth = 2;
+// ctx.strokeStyle = 'black'
+// ctx.beginPath();
+// ctx.moveTo(- c.width, 0);
+// ctx.lineTo(c.width, 0);
+// ctx.fillText('x', z*5-20, 20)
+// ctx.stroke();
+// // y
+// ctx.lineWidth = 2;
+// ctx.strokeStyle = 'black'
+// ctx.beginPath();
+// ctx.moveTo(0, - c.height);
+// ctx.lineTo(0, c.height);
+// ctx.stroke();
+
+
+// // сетка
+// ctx.lineWidth = 0.5;
+// ctx.beginPath();
+// for (let x = -6; x <6; x++) {
+//   ctx.moveTo(x*z, -c.height/2);
+//   ctx.lineTo(x*z, c.height/2);
+//   ctx.moveTo(-c.width/2, x*z);
+//   ctx.lineTo(c.width/2, x*z);
+// }
+// ctx.stroke();
+
+// // график функции
+// ctx.strokeStyle = "red";
+// ctx.lineWidth = 3;
+// ctx.beginPath();
+// for (var i = -6; i <= 6; i += 0.2) {
+//   ctx[i?'lineTo':'moveTo'](i*z, -y(i)*z);
+// }
+// ctx.stroke();
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'eval-cheap-source-map',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      // {
+      //   test: /\.(png|jpg|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'url-loader',
+      //       options: {
+      //         limit: 8192,
+      //       },
+      //     },
+      //   ],
+      // },
+      {
+        test: /\.svg/,
+        use: {
+          loader: "svg-url-loader",
+          options: {},
+        },
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+};
