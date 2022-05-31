b359a2ecad0544a6f94086fa15fc428f0dba1b79
Alexander Bychyk <sanya55bychyk@gmail.com>
2022-06-01 00:45:52 +0300

    feat: update drawGraph

--- src/scripts/darwGraph.js
+++ src/scripts/darwGraph.js
@@ -14,83 +14,87 @@ export default function drawGraph(key) {
   switch (key) {
     case 0:
       y = function(x) {
-        return inputs[0].value * x + inputs[1].value;
+        return Number(+inputs[0].value * x + +inputs[1].value).toFixed(2);
       }
+      drawGraphNormal()
       break;
     case 1:
       y = function(x) {
-        return inputs[0].value * Math.pow(x, 2) + inputs[1].value * x + inputs[2].value;
+        return Number(+inputs[0].value * Math.pow(x, 2) + +inputs[1].value * x + +inputs[2].value);
       }
+      drawGraphNormal()
       break;
     case 2:
       y = function(x) {
-        return Math.pow(x, inputs[0].value);
+
+        let res;
+        if (x > 0) {
+          res = Number(Math.pow(x.toFixed(2), +inputs[0].value)).toFixed(2);
+        } else {
+          res = -1 * Number(Math.pow(-(x).toFixed(2), +inputs[0].value)).toFixed(2)
+          console.log(-(x).toFixed(2), Math.pow(-(x).toFixed(2), +inputs[0].value));
+        }
+        return res;
       }
+      drawGraphNormal()
       break;
     case 3:
       y = function(x) {
-        return Math.pow(inputs[0].value, x);
+        return Number(Math.pow(+inputs[0].value, x)).toFixed(2);
       }
+      drawGraphNormal()
       break;
-    case 4:
+    case 4: 
       y = function(x) {
-        return Math.log(x) / Math.log(inputs[0].value);
+        return Number(Math.log(x) / Math.log(+inputs[0].value)).toFixed(2);
       }
+      drawGraphNormal()
       break;
     case 5:
       y = function(x) {
         return Math.sin(x);
       }
+      drawGraphPi()
       break;
     case 6:
       y = function(x) {
         return Math.cos(x);
       }
+      drawGraphPi()
       break;
     case 7:
       y = function(x) {
         return Math.tan(x);
       }
+      drawGraphPi()
       break;
     case 8:
       y = function(x) {
-        return 1 / Math.tan(x);
+        return Number(1 / Math.tan(x)).toFixed(2);
       }
+      drawGraphPi()
       break;
   }
 
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = 3;
-  ctx.beginPath();
-  for (var i = -7; i <= 7; i += 0.2) {
-    ctx[i?'lineTo':'moveTo'](i*scale, -y(i)*scale);
+  function drawGraphNormal() {
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    for (var i = -7; i <= 7; i += 0.1) {
+        ctx.lineTo(i*scale, -y(i)*scale);
+    }
+    ctx.stroke();
   }
-  ctx.stroke();
-  
-  
-  // var axes={};
-  // axes.x0 = .5 + .5*canvas.width;  // x0 pixels from left to x=0
-  // axes.y0 = .5 + .5*canvas.height; // y0 pixels from top to y=0
-  // axes.scale = 40;                 // 40 pixels from x=0 to x=1
-  // axes.doNegativeX = true;
-  // funGraph(ctx,axes,y,"rgb(11,153,11)",1);
 
-  // function funGraph (ctx,axes,func,color,thick) {
-  //   var xx, yy, dx=4, x0=axes.x0, y0=axes.y0, scale=axes.scale;
-  //   var iMax = Math.round((ctx.canvas.width-x0)/dx);
-  //   var iMin = axes.doNegativeX ? Math.round(-x0/dx) : 0;
-  //   ctx.beginPath();
-  //   ctx.lineWidth = thick;
-  //   ctx.strokeStyle = color;
-   
-  //   for (var i=iMin;i<=iMax;i++) {
-  //    xx = dx*i; yy = scale*func(xx/scale);
-  //    if (i==iMin) ctx.moveTo(x0+xx,y0-yy);
-  //    else         ctx.lineTo(x0+xx,y0-yy);
-  //   }
-  //   ctx.stroke();
-  //  }
-
-  
-
+  function drawGraphPi(i) {
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    for (let i = -7; i <= 7; i += 0.1) {
+      let x = Number(i*3.14*0.5).toFixed(2);
+      console.log(x)
+      ctx.lineTo(i*scale, -y(x)*scale);
+    }
+    ctx.stroke();
+  }
 }
